363f7c235e74db182cceec4e3acd903c18149f71
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2024-06-04 14:12:45 +0200

    bump deps

--- server/db/index.ts
+++ server/db/index.ts
@@ -4,10 +4,7 @@ import { env } from "@/env";
 import * as schema from "./schema";
 
 const client = neon(env.DATABASE_URI);
-/**
- * @todo remove ts-ignore once drizzle fixes this
- */
-//@ts-ignore
+
 export const db = drizzle(client, {
   schema,
 });
